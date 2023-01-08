d150da82f955ccf28d58d8cd9ec5cc472f982687
pl.max03 <pavliushyn.max.kp03@gmail.com>
2023-01-08 16:49:31 +0200

    make first functions

--- game/src/App.js
+++ game/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useEffect, useState } from 'react'
+const App = () => {
+  const [userChoice, setUserChoice] = useState(null)
+  const handleClick = (value) => {
+    setUserChoice(value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>user choice is {userChoice}</h1>
+      <h1>computer choice is </h1>
+      <button onClick={() => handleClick('rock')}>Rock</button>
+      <button onClick={() => handleClick('paper')}>Paper</button>
+      <button onClick={() => handleClick('scissors')}>Scissors</button>
     </div>
-  );
+
+  )
 }
 
 export default App;
