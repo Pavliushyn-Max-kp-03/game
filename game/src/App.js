be46030840fa7bc559edd0a53a520312742adaf9
pl.max03 <pavliushyn.max.kp03@gmail.com>
2023-01-08 17:00:47 +0200

    make user choice

--- game/src/App.js
+++ game/src/App.js
@@ -1,16 +1,15 @@
 import { useEffect, useState } from 'react'
 const App = () => {
   const [userChoice, setUserChoice] = useState(null)
+  const choices = ['rock', 'paper', 'scissors']
   const handleClick = (value) => {
     setUserChoice(value)
   }
   return (
     <div>
-      <h1>user choice is {userChoice}</h1>
-      <h1>computer choice is </h1>
-      <button onClick={() => handleClick('rock')}>Rock</button>
-      <button onClick={() => handleClick('paper')}>Paper</button>
-      <button onClick={() => handleClick('scissors')}>Scissors</button>
+      <h1>user choice is: {userChoice}</h1>
+      <h1>computer choice is: </h1>
+      {choices.map((choice, index) => <button key = {index} onClick={() => handleClick(choice)}>{choice}</button>)}
     </div>
 
   )
