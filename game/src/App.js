4f76f752ff071381d8833ef03daa812599ce32eb
pl.max03 <pavliushyn.max.kp03@gmail.com>
2023-01-10 12:37:51 +0200

    make computer choice

--- game/src/App.js
+++ game/src/App.js
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react'
 const App = () => {
   const [userChoice, setUserChoice] = useState(null)
+  const [computerChoice, setComputerChoice] = useState(null)
   const choices = ['rock', 'paper', 'scissors']
   const handleClick = (value) => {
     setUserChoice(value)
   }
+
+  const generateComputerChoice = () => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+    setComputerChoice(randomChoice)
+    generateComputerChoice()
+  }
   return (
     <div>
       <h1>user choice is: {userChoice}</h1>
-      <h1>computer choice is: </h1>
+      <h1>computer choice is: {computerChoice} </h1>
       {choices.map((choice, index) => <button key = {index} onClick={() => handleClick(choice)}>{choice}</button>)}
     </div>
 
